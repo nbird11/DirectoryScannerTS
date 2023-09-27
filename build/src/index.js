21b6563bb847992ae4058ad9bfaf46dd0017bd2b
Nathan Bird <nathanbirdka@gmail.com>
2023-09-26 19:52:31 -0600

    Main algorithm complete

--- build/src/index.js
+++ build/src/index.js
@@ -1,35 +1,102 @@
 "use strict";
+// import * as fs from 'node:fs';
 Object.defineProperty(exports, "__esModule", { value: true });
+// function listFiles(path = '.' /*, indent = 0*/): void {
+//     console.log(1);
+//     fs.readdir(path, (err, files) => {
+//         if (err) {
+//             console.log('ERROR:  ', err);
+//             return;
+//         }
+//         console.log(2);
+//         // files
+//         //     // .filter(f => f !== 'node_modules')
+//         //     .forEach(file => {
+//         //         fs.stat(path + '/' + file, (err, stats) => {
+//         //             if (err) {
+//         //                 console.log('ERROR:  ', err);
+//         //                 return;
+//         //             }
+//         //             if (stats.isFile()) {
+//         //                 console.log(indent, 'file:', path + '/' + file);
+//         //             } else if (stats.isDirectory()) {
+//         //                 console.log(indent, 'directory:', path + '/' + file);
+//         //                 listFiles(path + '/' + file, indent + 1);
+//         //             }
+//         //         });
+//         //     });
+//     });
+// }
+// function main(): void {
+//     listFiles();
+//     console.log(3);
+// }
+// main();
+// Async/Await version
 const fs = require("node:fs");
-function listFiles(path = '.' /*, indent = 0*/) {
-    let listOfFiles = [];
-    fs.readdir(path, (err, files) => {
-        if (err) {
-            console.log('ERROR:  ', err);
-            return;
-        }
-        console.log(listOfFiles);
-        listOfFiles.push(...files);
-        console.log(listOfFiles);
-        // files
-        //     // .filter(f => f !== 'node_modules')
-        //     .forEach(file => {
-        //         fs.stat(path + '/' + file, (err, stats) => {
-        //             if (err) {
-        //                 console.log('ERROR:  ', err);
-        //                 return;
-        //             }
-        //             if (stats.isFile()) {
-        //                 console.log(indent, 'file:', path + '/' + file);
-        //             } else if (stats.isDirectory()) {
-        //                 console.log(indent, 'directory:', path + '/' + file);
-        //                 listFiles(path + '/' + file, indent + 1);
-        //             }
-        //         });
-        //     });
-    });
-    return listOfFiles;
+const node_util_1 = require("node:util");
+const node_readline_1 = require("node:readline");
+const node_process_1 = require("node:process");
+const node_console_1 = require("node:console");
+const readdirAsync = (0, node_util_1.promisify)(fs.readdir);
+const statAsync = (0, node_util_1.promisify)(fs.stat);
+const rl = (0, node_readline_1.createInterface)({
+    input: node_process_1.stdin,
+    output: node_process_1.stdout,
+});
+function logFile(indent, file, i, length) {
+    console.log('│'.repeat(indent - 1) + (i !== length - 1 ? '├─' : '└─'), file);
 }
-const files = listFiles();
-// console.log(files);
+function logDir(indent, file) {
+    console.log('│'.repeat(indent - 1) + '├─', file + '\\');
+}
+async function listFiles(path, indent, showHidden) {
+    try {
+        const files = await readdirAsync(path);
+        if (indent === 1) {
+            const savedDir = process.cwd();
+            process.chdir(path);
+            console.log(process.cwd());
+            process.chdir(savedDir);
+        }
+        for (let i = 0; i < files.length; ++i) {
+            try {
+                if (files[i] === 'node_modules')
+                    continue; // TODO
+                if (!showHidden && files[i].startsWith('.'))
+                    continue;
+                const stats = await statAsync(path + '/' + files[i]);
+                if (stats.isFile()) {
+                    logFile(indent, files[i], i, files.length);
+                }
+                else if (stats.isDirectory()) {
+                    logDir(indent, files[i]);
+                    await listFiles(path + '/' + files[i], indent + 1, showHidden);
+                }
+                else {
+                    (0, node_console_1.assert)(false, 'There was something weird in a directory.');
+                }
+            }
+            catch (err) {
+                console.log('ERROR:\t', err);
+            }
+        }
+    }
+    catch (err) {
+        console.log('ERROR:\t', err);
+    }
+}
+async function main() {
+    const dir = await new Promise(resolve => {
+        rl.question(`Path from cwd (${process.cwd()}) to directory: `, resolve);
+    });
+    const hidden = await new Promise(resolve => {
+        rl.question('Show hidden files? (Y/n) ', resolve);
+    });
+    rl.close();
+    rl.removeAllListeners();
+    console.log();
+    await listFiles(dir, 1, hidden.toUpperCase() === 'y' ? true : false);
+}
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,115 @@
+// import * as fs from 'node:fs';
+
+// function listFiles(path = '.' /*, indent = 0*/): void {
+//     console.log(1);
+//     fs.readdir(path, (err, files) => {
+//         if (err) {
+//             console.log('ERROR:  ', err);
+//             return;
+//         }
+//         console.log(2);
+//         // files
+//         //     // .filter(f => f !== 'node_modules')
+//         //     .forEach(file => {
+//         //         fs.stat(path + '/' + file, (err, stats) => {
+//         //             if (err) {
+//         //                 console.log('ERROR:  ', err);
+//         //                 return;
+//         //             }
+
+//         //             if (stats.isFile()) {
+//         //                 console.log(indent, 'file:', path + '/' + file);
+//         //             } else if (stats.isDirectory()) {
+//         //                 console.log(indent, 'directory:', path + '/' + file);
+//         //                 listFiles(path + '/' + file, indent + 1);
+//         //             }
+//         //         });
+//         //     });
+//     });
+// }
+
+// function main(): void {
+//     listFiles();
+//     console.log(3);
+// }
+
+// main();
+
+// Async/Await version
 import * as fs from 'node:fs';
+import {promisify} from 'node:util';
+import {createInterface} from 'node:readline';
+import {stdin, stdout} from 'node:process';
+import {assert} from 'node:console';
 
-function listFiles(path = '.' /*, indent = 0*/): string[] {
-    let listOfFiles: string[] = [];
-    fs.readdir(path, (err, files) => {
-        if (err) {
-            console.log('ERROR:  ', err);
-            return;
-        }
-        console.log(listOfFiles);
-        listOfFiles.push(...files);
-        console.log(listOfFiles);
-        // files
-        //     // .filter(f => f !== 'node_modules')
-        //     .forEach(file => {
-        //         fs.stat(path + '/' + file, (err, stats) => {
-        //             if (err) {
-        //                 console.log('ERROR:  ', err);
-        //                 return;
-        //             }
+const readdirAsync = promisify(fs.readdir);
+const statAsync = promisify(fs.stat);
+const rl = createInterface({
+    input: stdin,
+    output: stdout,
+});
 
-        //             if (stats.isFile()) {
-        //                 console.log(indent, 'file:', path + '/' + file);
-        //             } else if (stats.isDirectory()) {
-        //                 console.log(indent, 'directory:', path + '/' + file);
-        //                 listFiles(path + '/' + file, indent + 1);
-        //             }
-        //         });
-        //     });
-    });
-    return listOfFiles;
+function logFile(indent: number, file: string, i: number, length: number) {
+    console.log(
+        '│'.repeat(indent - 1) + (i !== length - 1 ? '├─' : '└─'),
+        file
+    );
 }
 
-const files = listFiles();
-// console.log(files);
+function logDir(indent: number, file: string) {
+    console.log('│'.repeat(indent - 1) + '├─', file + '\\');
+}
+
+async function listFiles(
+    path: string,
+    indent: number,
+    showHidden: boolean
+): Promise<void> {
+    try {
+        const files = await readdirAsync(path);
+        if (indent === 1) {
+            const savedDir = process.cwd();
+            process.chdir(path);
+            console.log(process.cwd());
+            process.chdir(savedDir);
+        }
+        for (let i = 0; i < files.length; ++i) {
+            try {
+                if (files[i] === 'node_modules') continue; // TODO
+                if (!showHidden && files[i].startsWith('.')) continue;
+                const stats = await statAsync(path + '/' + files[i]);
+                if (stats.isFile()) {
+                    logFile(indent, files[i], i, files.length);
+                } else if (stats.isDirectory()) {
+                    logDir(indent, files[i]);
+                    await listFiles(
+                        path + '/' + files[i],
+                        indent + 1,
+                        showHidden
+                    );
+                } else {
+                    assert(false, 'There was something weird in a directory.');
+                }
+            } catch (err) {
+                console.log('ERROR:\t', err);
+            }
+        }
+    } catch (err) {
+        console.log('ERROR:\t', err);
+    }
+}
+
+async function main(): Promise<void> {
+    const dir = await new Promise<string>(resolve => {
+        rl.question(`Path from cwd (${process.cwd()}) to directory: `, resolve);
+    });
+    const hidden = await new Promise<string>(resolve => {
+        rl.question('Show hidden files? (Y/n) ', resolve);
+    });
+    rl.close();
+    rl.removeAllListeners();
+    console.log();
+    await listFiles(dir, 1, hidden.toUpperCase() === 'y' ? true : false);
+}
+
+main();
