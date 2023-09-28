cff77b3fc965f9b4eeebd752c55c41b866f2e697
Nathan Bird <nathanbirdka@gmail.com>
2023-09-28 09:06:06 -0600

    Add documentation and comments

--- build/src/index.js
+++ build/src/index.js
@@ -1,72 +1,100 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("node:fs");
-const node_util_1 = require("node:util");
-const node_readline_1 = require("node:readline");
-const node_process_1 = require("node:process");
-const node_console_1 = require("node:console");
-const readdirAsync = (0, node_util_1.promisify)(fs.readdir);
-const statAsync = (0, node_util_1.promisify)(fs.stat);
+const node_fs_1 = require("node:fs"); // For file system methods
+const node_util_1 = require("node:util"); // For making functions work synchronously
+const node_readline_1 = require("node:readline"); // For getting input from user
+const node_console_1 = require("node:console"); // For catching weird items in a directory
+// Create promisified versions of the fs functions for readdir and
+// stat so I can use await to get the results synchronously.
+const readdirAsync = (0, node_util_1.promisify)(node_fs_1.readdir);
+const statAsync = (0, node_util_1.promisify)(node_fs_1.stat);
+// Create the readline interface.
 const rl = (0, node_readline_1.createInterface)({
-    input: node_process_1.stdin,
-    output: node_process_1.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
-function logFile(indent, file, i, length) {
-    const isLastFile = i === length - 1;
-    console.log([...'│'.repeat(indent - 1)].join(' ') +
+/**
+ * Outputs a line containing the name of the file to the display.
+ * @param indent The depth of the file from root.
+ * @param file The name of the file.
+ * @param isLastFile Whether the file is the last one in the current directory.
+ */
+function logFile(indent, file, isLastFile) {
+    console.log(
+    // Indent is used to determine the number of vertical lines needed before the file.
+    [...'│'.repeat(indent - 1)].join(' ') +
         (indent > 1 ? ' ' : '') +
         ((isLastFile ? '└──' : '├──') + file));
 }
-function logDir(indent, file) {
+/**
+ * Outputs a line containing the name of the directory to the display.
+ * @param indent The depth of the dir from root.
+ * @param dir The name of the dir.
+ */
+function logDir(indent, dir) {
     console.log([...'│'.repeat(indent - 1)].join(' ') +
         (indent > 1 ? ' ' : '') +
-        ('├──' + `${file}/`));
+        ('├──' + `${dir}/`));
 }
+/**
+ * Recursive function that lists all the files and directories from the
+ * specified path.
+ * @param path Directory from which the files are read.
+ * @param indent Depth of files from root directory.
+ * @param showHidden Whether to show hidden files and directories or not.
+ */
 async function listFiles(path = '.', indent = 1, showHidden = false) {
     try {
         const files = await readdirAsync(path);
+        // On first pass, display the full path of specified relative path.
         if (indent === 1) {
+            // TODO Change to if (path.contains('.')); May be less stable
             const savedDir = process.cwd();
             process.chdir(path);
             console.log(process.cwd());
             process.chdir(savedDir);
         }
         for (let i = 0; i < files.length; ++i) {
-            try {
-                if (files[i] === 'node_modules')
-                    continue; // TODO
-                if (!showHidden && files[i].startsWith('.'))
-                    continue;
-                const stats = await statAsync(path + '/' + files[i]);
-                if (stats.isFile()) {
-                    logFile(indent, files[i], i, files.length);
-                }
-                else if (stats.isDirectory()) {
-                    logDir(indent, files[i]);
-                    await listFiles(path + '/' + files[i], indent + 1, showHidden);
-                }
-                else {
-                    (0, node_console_1.assert)(false, 'There was something weird in a directory.');
-                }
+            // node_modules is a huge directory so I'm skipping it by default.
+            if (files[i] === 'node_modules')
+                continue; // TODO
+            // A '.' at the beginning of a file/dir name usually denotes a hidden file.
+            if (!showHidden && files[i].startsWith('.'))
+                continue;
+            // Use fs.Stats to determine whether it's a file or directory.
+            const stats = await statAsync(path + '\\' + files[i]);
+            if (stats.isFile()) {
+                const lastFile = i === files.length - 1;
+                logFile(indent, files[i], lastFile);
             }
-            catch (err) {
-                console.log('ERROR:\t', err);
+            else if (stats.isDirectory()) {
+                logDir(indent, files[i]);
+                // Recurse to get files within directory.
+                // TODO Add option for depth at which to stop listing nested files.
+                await listFiles(path + '/' + files[i], indent + 1, showHidden);
+            }
+            else {
+                (0, node_console_1.assert)(false, 'There was something weird in a directory.');
             }
         }
     }
     catch (err) {
+        // Catches error with readdirAsync and statAsync.
         console.log('ERROR:\t', err);
     }
 }
 async function main() {
+    // Prompt for root dir.
     const dir = await new Promise(resolve => {
         rl.question('Path from cwd to directory: ', resolve);
     });
+    // Prompt for showing hidden files.
     const hidden = await new Promise(resolve => {
         rl.question('Show hidden files? (Y/n) ', resolve);
     });
     rl.close();
     rl.removeAllListeners();
+    // List all files recursively in the specified root dir.
     console.log();
     await listFiles(dir, 1, hidden.toUpperCase() === 'Y' ? true : false);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,110 @@
-import * as fs from 'node:fs';
-import {promisify} from 'node:util';
-import {createInterface} from 'node:readline';
-import {stdin, stdout} from 'node:process';
-import {assert} from 'node:console';
+import {readdir, stat} from 'node:fs'; // For file system methods
+import {promisify} from 'node:util'; // For making functions work synchronously
+import {createInterface} from 'node:readline'; // For getting input from user
+import {assert} from 'node:console'; // For catching weird items in a directory
 
-const readdirAsync = promisify(fs.readdir);
-const statAsync = promisify(fs.stat);
+// Create promisified versions of the fs functions for readdir and
+// stat so I can use await to get the results synchronously.
+const readdirAsync = promisify(readdir);
+const statAsync = promisify(stat);
+
+// Create the readline interface.
 const rl = createInterface({
-    input: stdin,
-    output: stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
-function logFile(indent: number, file: string, i: number, length: number) {
-    const isLastFile = i === length - 1;
+/**
+ * Outputs a line containing the name of the file to the display.
+ * @param indent The depth of the file from root.
+ * @param file The name of the file.
+ * @param isLastFile Whether the file is the last one in the current directory.
+ */
+function logFile(indent: number, file: string, isLastFile: boolean): void {
     console.log(
+        // Indent is used to determine the number of vertical lines needed before the file.
         [...'│'.repeat(indent - 1)].join(' ') +
             (indent > 1 ? ' ' : '') +
             ((isLastFile ? '└──' : '├──') + file)
     );
 }
 
-function logDir(indent: number, file: string) {
+/**
+ * Outputs a line containing the name of the directory to the display.
+ * @param indent The depth of the dir from root.
+ * @param dir The name of the dir.
+ */
+function logDir(indent: number, dir: string): void {
     console.log(
         [...'│'.repeat(indent - 1)].join(' ') +
             (indent > 1 ? ' ' : '') +
-            ('├──' + `${file}/`)
+            ('├──' + `${dir}/`)
     );
 }
 
+/**
+ * Recursive function that lists all the files and directories from the
+ * specified path.
+ * @param path Directory from which the files are read.
+ * @param indent Depth of files from root directory.
+ * @param showHidden Whether to show hidden files and directories or not.
+ */
 async function listFiles(
     path = '.',
     indent = 1,
     showHidden = false
 ): Promise<void> {
     try {
-        const files = await readdirAsync(path);
+        const files: string[] = await readdirAsync(path);
+
+        // On first pass, display the full path of specified relative path.
         if (indent === 1) {
+            // TODO Change to if (path.contains('.')); May be less stable
             const savedDir = process.cwd();
             process.chdir(path);
             console.log(process.cwd());
             process.chdir(savedDir);
         }
         for (let i = 0; i < files.length; ++i) {
-            try {
-                if (files[i] === 'node_modules') continue; // TODO
-                if (!showHidden && files[i].startsWith('.')) continue;
-                const stats = await statAsync(path + '/' + files[i]);
-                if (stats.isFile()) {
-                    logFile(indent, files[i], i, files.length);
-                } else if (stats.isDirectory()) {
-                    logDir(indent, files[i]);
-                    await listFiles(
-                        path + '/' + files[i],
-                        indent + 1,
-                        showHidden
-                    );
-                } else {
-                    assert(false, 'There was something weird in a directory.');
-                }
-            } catch (err) {
-                console.log('ERROR:\t', err);
+            // node_modules is a huge directory so I'm skipping it by default.
+            if (files[i] === 'node_modules') continue; // TODO
+
+            // A '.' at the beginning of a file/dir name usually denotes a hidden file.
+            if (!showHidden && files[i].startsWith('.')) continue;
+
+            // Use fs.Stats to determine whether it's a file or directory.
+            const stats = await statAsync(path + '\\' + files[i]);
+            if (stats.isFile()) {
+                const lastFile: boolean = i === files.length - 1;
+                logFile(indent, files[i], lastFile);
+            } else if (stats.isDirectory()) {
+                logDir(indent, files[i]);
+                // Recurse to get files within directory.
+                // TODO Add option for depth at which to stop listing nested files.
+                await listFiles(path + '/' + files[i], indent + 1, showHidden);
+            } else {
+                assert(false, 'There was something weird in a directory.');
             }
         }
     } catch (err) {
+        // Catches error with readdirAsync and statAsync.
         console.log('ERROR:\t', err);
     }
 }
 
 async function main(): Promise<void> {
+    // Prompt for root dir.
     const dir = await new Promise<string>(resolve => {
         rl.question('Path from cwd to directory: ', resolve);
     });
+    // Prompt for showing hidden files.
     const hidden = await new Promise<string>(resolve => {
         rl.question('Show hidden files? (Y/n) ', resolve);
     });
     rl.close();
     rl.removeAllListeners();
 
+    // List all files recursively in the specified root dir.
     console.log();
     await listFiles(dir, 1, hidden.toUpperCase() === 'Y' ? true : false);
 }
